6ea43a4d97c84d6ca1426736e6d867718c1b7feb
TramMai01 <maitram570@gmail.com>
2024-05-12 09:35:37 +0700

    Viết code xử lý tạo đề tài

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const teacherController = require('../controllers/teacherController')
-
-
 router.get('/', teacherController.getTeacher) 
 router.get('/projects/create', teacherController.getCreateProject)
 router.post('/projects/create', teacherController.createProject)
