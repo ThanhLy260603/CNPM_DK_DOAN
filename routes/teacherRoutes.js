6a4cb104f65e481e23ab91ff7dd7ae0fe3ca7dce
SichithienDa21tta <sichithien2020@gmail.com>
2024-05-26 10:06:50 +0700

    Xây dựng giao diện trang đăng ký đề tài

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -4,18 +4,24 @@ const teacherController = require('../controllers/teacherController')
 
 
 router.get('/', teacherController.getTeacher) 
-router.get('/projects/create', teacherController.getCreateProject)
-router.post('/projects/create', teacherController.createProject)
-router.get('/projects', teacherController.getProjects)
-router.get('/projects/:id', teacherController.getEditProject)
-router.post('/projects/:id', teacherController.editProject)
-router.get('/projects/approval', teacherController.getApproval)
-router.get('/projects/approval/:id', teacherController.getApprovalDetail)
-router.post('/projects/approval/:id', teacherController.approvalDetail)
 router.get('/students/create', teacherController.getCreateStudent)
 router.post('/students/create', teacherController.createStudent)
 router.get('/students', teacherController.getStudents)
+
+
+// project  
+router.get('/projects/create', teacherController.getCreateProject)
+router.post('/projects/create', teacherController.createProject)
+router.get('/projects', teacherController.getProjects)
+router.get('/projects/approval', teacherController.getApproval)
+router.get('/logout', teacherController.handleLogout)
+router.get('/projects/approval/:id', teacherController.getApprovalDetail)
+router.post('/projects/approval/:id', teacherController.approvalDetail)
+
+// edit sinh viên
 router.get('/students/:id', teacherController.getEditStudent)
 router.post('/students/:id', teacherController.editStudent)
-router.get('/logout', teacherController.handleLogout)
+router.get('/projects/:id', teacherController.getEditProject)
+router.post('/projects/:id', teacherController.editProject)
+
 module.exports = router
