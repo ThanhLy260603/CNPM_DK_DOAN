471df02b7fa3751ef745c1dcfb499fac4edf10da
TramMai01 <maitram570@gmail.com>
2024-05-28 19:00:42 +0700

    Viết code xử lý đăng ký đề tài

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -4,5 +4,6 @@ const studentController = require('../controllers/studentController')
 
 router.get('/', studentController.getStudents)
 router.get('/projects', studentController.getRegisterProject)
+router.post('/projects', studentController.registerProject)
 module.exports = router
 
